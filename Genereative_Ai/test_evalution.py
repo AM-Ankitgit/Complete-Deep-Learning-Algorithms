629199de8327a1b957f5302851e4a5c29513f324
AM-Ankitgit <mahalleankit007@gmail.com>
2025-05-01 18:10:50 +0530

    file updated

--- Genereative_Ai/test_evalution.py
+++ Genereative_Ai/test_evalution.py
@@ -10,3 +10,20 @@ bleu = sentence_bleu([reference.split()], generated.split())
 
 print("ROUGE Score:", scores)
 print("BLEU Score:", bleu)
+
+
+
+
+
+
+from rouge import Rouge
+from nltk.translate.bleu_score import sentence_bleu
+
+rouge1 = Rouge()
+
+scores = rouge1.get_scores(generated,reference)
+print((scores))
+
+sentence_bleu = sentence_bleu([generated.split()],reference.split())
+print(sentence_bleu)
+
